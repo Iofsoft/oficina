d9fd6a8c8a836b8ddcfec76d6164dd9ca2a5c16e
Ian Fernandes <134323322+Iofsoft@users.noreply.github.com>
2024-10-04 23:14:12 -0300

    Update OficinaApplication.java
    Adicionando nome e json

--- src/main/java/com/prw3/oficina/OficinaApplication.java
+++ src/main/java/com/prw3/oficina/OficinaApplication.java
@@ -1,3 +1,24 @@
+//Nome: Ian de Oliveira Fernandes
+//RA: SC3037215
+/*
+Json usado para cadastrar um conserto:
+
+	{
+		"dataEntrada": "01/10/2024",
+		"dataSaida": "04/10/2024",
+		"mecanicoDados": {
+			"nome": "Jovem da Silva",
+			"experiencia": 2
+		},
+		"veiculoDados": {
+			"marca": "Mercedes",
+			"modelo": "G63",
+			"ano" : "2025"
+		}
+	}
+		
+*/
+
 package com.prw3.oficina;
 
 import org.springframework.boot.SpringApplication;
